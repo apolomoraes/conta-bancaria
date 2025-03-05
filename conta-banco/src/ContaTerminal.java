5370a20a6116e6c3553157bc8c279256da27279b
Apolo Moraes <vlogsapolo@gmail.com>
2025-03-05 08:39:30 -0300

    finished project

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome do titular!");
+        String titular = scanner.next();
+
+        System.out.println("Por favor, digite o saldo!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + titular + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
